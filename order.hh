5f5cf8ecf7f29bf054fec81f50ba53e64180768c
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-08 18:04:07 -0500

    Modified definitions

--- order.hh
+++ order.hh
@@ -6,8 +6,16 @@ class Order{
 public:
     unsigned short order_id;
     std::array<char,16> company;
-    float price;
-    short quantity; // positive=buy, negative=sell
+    float quote_price; // positive=buy, negative=sell
+    short quantity; 
     char all_or_none; // Reserved, char used as bool
     std::chrono::time_point<std::chrono::system_clock> timestamp;
 };
+
+class CompletedOrder{
+public:
+    unsigned short order_id;
+    float final_price; // price at which a transaction was done
+    short quantity; // In case of partial order
+    std::chrono::time_point<std::chrono::system_clock> timestamp; // when the transaction was completed; -- not sure if usefull
+}
--- orderbook.hh
+++ orderbook.hh
@@ -5,14 +5,15 @@
 
 struct Comp{ // Counterpart of std::less
     constexpr bool operator()(const Order &a, const Order &b)const{
-        return a.price<b.price || (a.price==b.price&&a.timestamp<b.timestamp);
+        return a.quote_price<b.quote_price || \
+            (a.quote_price==b.quote_price&&a.timestamp<b.timestamp);
     }
 };
 
 class OrderBook{
 private:
     std::priority_queue<Order, std::vector<Order>, Comp> buypool, sellpool; // maxheap: highest buy price, lowest sell price
-
+    std::vector<CompletedOrder> match();
 public:
     int add_order(Order);
 };
