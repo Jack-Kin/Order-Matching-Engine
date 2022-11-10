0779790b82479957f291cd7dbd47b49d4498ac50
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-10 16:27:13 -0500

    Update order.hh

--- order.hh
+++ order.hh
@@ -2,20 +2,28 @@
 #include <array>
 #include <chrono>
 
+enum class OrderType : char {
+    limit,
+    market,
+    stop,
+    stop_limit
+};
+
 class Order{
 public:
     unsigned short order_id;
-    std::array<char,16> company;
-    float quote_price; // positive=buy, negative=sell
-    short quantity; 
-    char all_or_none; // Reserved, char used as bool
+    unsigned short quantity; 
+    unsigned quote;
+    OrderType order_type;
+    char is_buy; // buy=1, sell=0
+    char all_or_none; // aon=1, partial order allowed=0
     std::chrono::time_point<std::chrono::system_clock> timestamp;
 };
 
 class CompletedOrder{
 public:
     unsigned short order_id;
-    float final_price; // price at which a transaction was done
-    short quantity; // In case of partial order
+    unsigned final_price; // price at which a transaction was done
+    unsigned short quantity; // In case of partial order
     std::chrono::time_point<std::chrono::system_clock> timestamp; // when the transaction was completed; -- not sure if usefull
 }
