1e2595d78ea4ea30091d96aed1c3628445b0c978
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-12 16:43:18 -0500

    Update orderbook.hh

--- orderbook.hh
+++ orderbook.hh
@@ -1,42 +1,45 @@
 #pragma once
-// #include <queue>
-#include <deque>
-#include <limits>
-#include <map>
+
+#include <array>
+#include <list>
+#include <optional>
+#include <set>
 #include <unordered_map>
 
 #include "order.hh"
-#include <optional>
 
 class OrderBook{
 private:
-    std::map<float,std::deque<Order>,std::greater<float>> buypool;
-    std::map<float,std::deque<Order>> sellpool;
-    // If orders in a level are deleted, we cannot delete it from the priority queue
-    // Hence reverting to map for buy/sell pool, we can revise data structure in v 1.0
-	// std::priority_queue<float> buyprices;
-    // std::priority_queue<float, vector<float>, std::greater<float>> sellprices;
+    std::array<char,16> symbol;
+    std::unordered_map<unsigned, std::list<Order>> buypool, sellpool; // key=price level; value=a list of Order
+    std::set<unsigned> buyprices, sellprices; // stores current levels of the hashmaps (buypool and sellpool)
+    std::unordered_map<unsigned, unsigned> priceofID; // key=order ID, value=price level
+
+    // std::map is slower than std::unordered_map, see complexity below.
+    // std::map<float,std::deque<Order>,std::greater<float>> buypool;
+    // std::map<float,std::deque<Order>> sellpool;
     // Adding a map from order id to order object reference since we cannot identify order type just with price level info
-	std::unordered_map<unsigned int, Order&> order_map;
+	// std::unordered_map<unsigned int, Order&> order_map;
+
     void add_market_order(Order&);
     void add_limit_order(Order&);
-    // std::vector<Transaction> match_limit(Order &order);
-    // std::vector<Transaction> match_market(Order &order);
+    std::vector<Transaction> match_limit(Order &order);
+    std::vector<Transaction> match_market(Order &order);
 public:
     void add_order(Order&);
     std::optional<Order> get_order(unsigned int);
     bool delete_order(unsigned int);
-    void printBuySellPool();
+    void printBuySellPool()const;
 };
 
 /*Complexity:
+N orders; M orders per price
 
-n = no of orders
 Add order: 
-Add to buy/sellpool - O(log n) 
-Add to order_map - O(1)
+Add to buy/sellpool - average O(M); O(M + log N) if using std::map
+// Add to order_map - O(1)
 
 Delete order: 
-delete from buy/sell pool -O(log n)
-delete from order map - O(1)
+delete from buy/sell pool - average O(M); O(log N) ? if using order_map
+// delete from order map - O(1)
 */
