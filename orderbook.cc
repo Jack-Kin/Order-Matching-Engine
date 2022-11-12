a9fbe58a2a8659df31aab6c7fd27b4bd47958fc5
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-11 20:08:58 -0500

    Updated function arguments

--- orderbook.cc
+++ orderbook.cc
@@ -1,11 +1,9 @@
 #include "orderbook.hh"
 
-std::vector<CompletedOrder> OrderBook::match(){
-    while(!buypool.empty()&&!sellpool.empty()&&buypool.top().quote_price+sellpool.top().quote_price>0){
-        
-    }
+std::vector<Transaction> OrderBook::match_market(Order &order){
+    
 }
 
-int OrderBook::add_order(Order order){
+int OrderBook::add_order(Order &order){
     
 }
--- orderbook.hh
+++ orderbook.hh
@@ -18,10 +18,10 @@ private:
     std::unordered_map<unsigned, std::list<Order>> buypool, sellpool; // key=price level; value=a list of Order
     std::priority_queue<unsigned> buyprices, sellprices; // stores current levels of the hashmaps (buypool and sellpool)
     std::unordered_map<unsigned, unsigned> priceofID; // key=order ID, value=price level
-    std::vector<Transaction> match_limit(Order order);
-    std::vector<Transaction> match_market(Order order);
+    std::vector<Transaction> match_limit(Order &order);
+    std::vector<Transaction> match_market(Order &order);
 public:
-    int add_order(Order order);
+    int add_order(Order &order);
     int view_order();
     int remove_order(unsigned order_id);
 };
