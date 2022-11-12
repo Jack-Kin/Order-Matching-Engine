c77d8b36d1876909f2da46f32947bd667241575f
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-12 14:20:22 -0500

    Update order.hh

--- order.hh
+++ order.hh
@@ -22,16 +22,18 @@ enum class OrderType : unsigned char {
 
 class Order{
 private:
-    unsigned short order_id;
-    unsigned short quantity; 
+    unsigned order_id;
+    unsigned owner_id;
+    unsigned quantity; 
     unsigned quote;
     OrderSide order_side;
     OrderType order_type;
     char all_or_none; // aon=1, partial order allowed=0
     std::chrono::time_point<std::chrono::system_clock> timestamp;
 public:
-    Order(unsigned short id, unsigned short qty, unsigned quote, OrderSide sd, OrderType tp, char aon, std::chrono::time_point<std::chrono::system_clock> tmstmp):
-        order_id(id),
+    Order(unsigned order, unsigned owner, unsigned qty, unsigned quote, OrderSide sd, OrderType tp, char aon, std::chrono::time_point<std::chrono::system_clock> tmstmp):
+        order_id(order),
+        owner_id(owner),
         quantity(qty),
         quote(quote),
         order_side(sd),
@@ -39,8 +41,9 @@ public:
         all_or_none(aon),
         timestamp(tmstmp){}
     auto get_id()const{return order_id;}
+    auto get_owner()const{return owner_id;}
     auto get_quantity()const{return quantity;}
-    void reduce_quantity(unsigned short x){quantity-=x;} // only if aon=0
+    void reduce_quantity(unsigned x){quantity-=x;} // only if aon=0
     auto get_quote()const{return quote;}
     void set_quote(unsigned x){quote=x;} // only for market orders
     auto get_side()const{return order_side;}
@@ -58,9 +61,9 @@ std::ostream& operator<<(std::ostream &s, const Order &order) {
 
 class Transaction{
 public:
-    unsigned short buyside_order_id;
-    unsigned short sellside_order_id;
+    unsigned buyside_order_id;
+    unsigned sellside_order_id;
     unsigned final_price; // price at which a transaction was done
-    unsigned short quantity; // In case of partial order
+    unsigned quantity; // In case of partial order
     std::chrono::time_point<std::chrono::system_clock> timestamp; // when the transaction was completed; -- not sure if useful
 };
