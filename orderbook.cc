f247a8f0d07ce1479c62d2cf3ae0f7a5964772b5
FungluiKoo <fenglei.gu@nyu.edu>
2022-11-12 16:58:38 -0500

    Moved the test to separate file

--- orderbook.cc
+++ orderbook.cc
@@ -2,7 +2,7 @@
 
 
 void OrderBook::add_order(Order& order){
-    switch(order.order_type){
+    switch(order.get_type()){
         case OrderType :: MARKET:
             add_market_order(order);
             break;
@@ -13,28 +13,28 @@ void OrderBook::add_order(Order& order){
 }
 
 void OrderBook::add_limit_order(Order& order){
-    if(order.order_side == OrderSide ::BUY){
-        // if (buypool.find(order.price) == buypool.end()){ //price level not found
-        //     buyprices.push(order.price);
+    if(order.get_side() == OrderSide ::BUY){
+        // if (buypool.find(order.get_quote()) == buypool.end()){ //price level not found
+        //     buyprices.push(order.get_quote());
         // }
-        buypool[order.price].push_back(order);
+        buypool[order.get_quote()].push_back(order);
     } else{
-        // if (sellpool.find(order.price) == sellpool.end()){ //price level not found
-        //     sellprices.push(order.price);
+        // if (sellpool.find(order.get_quote()) == sellpool.end()){ //price level not found
+        //     sellprices.push(order.get_quote());
         // }
-        sellpool[order.price].push_back(order); 
+        sellpool[order.get_quote()].push_back(order); 
     }
     order_map.insert({order.order_id,order});
 }
 
 void OrderBook::add_market_order(Order& order){
-    if(order.order_side == OrderSide ::BUY){
+    if(order.get_side() == OrderSide ::BUY){
         //check top of sellprices for best sell price, if not exists set to max
-        order.price = std::numeric_limits<float>::max();
-        buypool[order.price].push_back(order);
+        order.set_quote(std::numeric_limits<float>::max());
+        buypool[order.get_quote()].push_back(order);
     } else{
-        order.price = 0.0;
-        sellpool[order.price].push_back(order); 
+        order.set_quote(0.0);
+        sellpool[order.get_quote()].push_back(order); 
     }
     order_map.insert({order.order_id,order});
 }
@@ -56,26 +56,26 @@ bool OrderBook::delete_order(unsigned int order_id){
     }
     Order order = *order_obj;
     order_map.erase(order_id);
-    // check buypool and delete from deque
-    std::deque<Order> order_list;
-    if(order.order_side == OrderSide ::BUY){
-        order_list = buypool[order.price];
+    // check buypool and delete from list
+    std::list<Order> order_list;
+    if(order.get_side() == OrderSide ::BUY){
+        order_list = buypool[order.get_quote()]; // this is just a copy (also taking a lot of time)
         for (auto it = order_list.begin(); it != order_list.end(); ++it) {
         if(it->order_id == order_id){
             order_list.erase(it);
             if(order_list.empty()){ 
-                buypool.erase(order.price);
+                buypool.erase(order.get_quote()); // deleted the object in the copy, but not the original one.
             }
             return true;
         }
     }   
     }else{
-        order_list = sellpool[order.price];
+        order_list = sellpool[order.get_quote()];
         for (auto it = order_list.begin(); it != order_list.end(); ++it) {
         if(it->order_id == order_id){
             order_list.erase(it);
             if(order_list.empty()){ 
-                sellpool.erase(order.price);
+                sellpool.erase(order.get_quote());
             }
             return true;
         }
@@ -104,34 +104,3 @@ void OrderBook::printBuySellPool()
 // std::vector<Transaction> OrderBook::match_market(Order &order){
     
 // }
-
-int main(){
-
-    OrderBook book;
-    unsigned int order_id = 1;
-    std::array<char,16> owner = {"ABC"};
-    double price = 1000.0;
-    // uint32_t qty = 5;
-    Order buy1(1,owner,'B','M',1000.0,5,' ',std::chrono::system_clock::now());
-    Order buy2(2,owner,'B','M',800.0,10,' ',std::chrono::system_clock::now());
-    Order buy3(3,owner,'B','L',600.0,5,' ',std::chrono::system_clock::now());
-
-    Order sell1(4,owner,'S','M',1000.0,5,' ',std::chrono::system_clock::now());
-    Order sell2(5,owner,'S','L',800.0,10,' ',std::chrono::system_clock::now());
-    Order sell3(6,owner,'S','L',600.0,5,' ',std::chrono::system_clock::now());
-    // Order buy2("2","ABC",'B',800.0,5,std::chrono::system_clock::now());
-    // Order buy3("3","ABC",'B',900.0,5,std::chrono::system_clock::now());
-    book.add_order(buy1);
-    book.add_order(buy2);
-    book.add_order(buy3);
-
-    book.add_order(sell1);
-    book.add_order(sell2);
-    book.add_order(sell3);
-
-    book.printBuySellPool();
-    // std::cout << "fetch order 1 " << book.get_order(1) << std::endl;
-    book.delete_order(6);
-    book.delete_order(10); // shouldn't delete
-    book.printBuySellPool();
-}
---
+++ orderbook_test.cc
@@ -0,0 +1,32 @@
+#include "orderbook.hh"
+
+int main(){
+
+    OrderBook book;
+    unsigned int order_id = 1;
+    std::array<char,16> owner = {"ABC"};
+    double price = 1000.0;
+    // uint32_t qty = 5;
+    Order buy1(1,owner,'B','M',1000.0,5,' ',std::chrono::system_clock::now());
+    Order buy2(2,owner,'B','M',800.0,10,' ',std::chrono::system_clock::now());
+    Order buy3(3,owner,'B','L',600.0,5,' ',std::chrono::system_clock::now());
+
+    Order sell1(4,owner,'S','M',1000.0,5,' ',std::chrono::system_clock::now());
+    Order sell2(5,owner,'S','L',800.0,10,' ',std::chrono::system_clock::now());
+    Order sell3(6,owner,'S','L',600.0,5,' ',std::chrono::system_clock::now());
+    // Order buy2("2","ABC",'B',800.0,5,std::chrono::system_clock::now());
+    // Order buy3("3","ABC",'B',900.0,5,std::chrono::system_clock::now());
+    book.add_order(buy1);
+    book.add_order(buy2);
+    book.add_order(buy3);
+
+    book.add_order(sell1);
+    book.add_order(sell2);
+    book.add_order(sell3);
+
+    book.printBuySellPool();
+    // std::cout << "fetch order 1 " << book.get_order(1) << std::endl;
+    book.delete_order(6);
+    book.delete_order(10); // shouldn't delete
+    book.printBuySellPool();
+}
