f8a445b1752f47b42776185db751c47a9a309a42
FungluiKoo <fenglei.gu@nyu.edu>
2022-12-09 02:28:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     std::cout << "Welcome to the Nasdaq ITCH order matching engine" << std::endl;
 
-    string file_path = "../ExchangeDataViewer/data/03272019.PSX_ITCH50";
+    string file_path = "./data/03272019.PSX_ITCH50";
     string outputMessageCSV = "./data/test.log";
 
 
